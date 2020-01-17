3ecba3ce5ef523978f70703e1e23769961f9b1df
Brenton <bren10@gmail.com>
2020-01-17 00:05:48 -0800

    Car Insurance App

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,9 +4,22 @@ namespace ConsoleApp5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine("Car Insurance Program");
+            Console.Write("What's your age? ");
+            string age_input = Console.ReadLine();
+            int age = Convert.ToInt32(age_input);
+            Console.Write("Have you ever had a DUI? (enter true or false): ");          
+            string dui_input = Console.ReadLine();
+            bool dui = Convert.ToBoolean(dui_input);
+            Console.Write("How many tickets have you had? ");
+            string num_tickets_input = Console.ReadLine();
+            int ticket_num = Convert.ToInt32(num_tickets_input);
+            bool qualified = (age > 15) && (dui == false) && (ticket_num <= 3);
+            Console.WriteLine("Qualified?");
+            Console.Write(qualified);
+
         }
     }
 }
