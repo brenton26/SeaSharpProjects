16b8e3a9c681a6a8e47da8a1d98ae436f54d8d7a
Brenton <bren10@gmail.com>
2020-01-26 21:19:26 -0800

    new console apps added

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Kyle", 1));
+            employees.Add(new Employee("Brenton", 2));
+            employees.Add(new Employee("Brion", 3));
+            employees.Add(new Employee("Kelly", 4));
+            employees.Add(new Employee("Casey", 5));
+            employees.Add(new Employee("Chris", 6));
+            employees.Add(new Employee("Josh", 7));
+            employees.Add(new Employee("Jeff", 8));
+            employees.Add(new Employee("Taylor", 9));
+            employees.Add(new Employee("Ben", 10));
+            employees.Add(new Employee("Jared", 11));
+            employees.Add(new Employee("Elizabeth", 12));
+            employees.Add(new Employee("Shannon", 13));
+            employees.Add(new Employee("Nancy", 14));
+            employees.Add(new Employee("Casey", 15));
+
+            List<Employee> Casey_List = new List<Employee>();
+            foreach (Employee employee in employees)
+            {
+                if (employee.Name == "Casey")
+                {
+                    Casey_List.Add(employee);
+                }
+            }
+            foreach (Employee employee in Casey_List)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            List<Employee> Casey_List2 = employees.Where(x => x.Name == "Casey" ).ToList();
+            foreach (Employee employee in Casey_List2)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            List<Employee> Even_Id = employees.Where(x => x.ID % 2 == 0).ToList();
+            foreach (Employee employee in Even_Id)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+    public class Employee
+    {
+        public Employee(string name, int id)
+        {
+            this.Name = name;
+            this.ID = id;
+            }
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+
+
+}
--- TwentyOneCardGame/Program.cs
+++ TwentyOneCardGame/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
-
+using System.Linq;
 
 namespace TwentyOneCardGame
 {
@@ -12,12 +11,22 @@ namespace TwentyOneCardGame
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.Clear();
 
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Rank = Rank.Ace;
-            card2.Rank = Rank.Two;
-            Console.WriteLine("Card 1: " + card1.Rank);
-            Console.WriteLine("Card 2: " + card2.Rank);
+            FullDeck deck = new FullDeck();
+            List<Card> kingz = deck.Cards.Where(x => x.Rank == Rank.Jack ).ToList();
+            foreach (Card card in kingz) {
+                Console.WriteLine(card.Suit);
+            }
+
+            List<int> my_list = new List<int> { 2, 5, 10, 25, 26, 42, 64, 69 };
+            int my_int = my_list.Sum(x => x*x);
+            Console.WriteLine(my_int);
+
+            //Card card1 = new Card();
+            //Card card2 = card1;
+            //card1.Rank = Rank.Ace;
+            //card2.Rank = Rank.Two;
+            //Console.WriteLine("Card 1: " + card1.Rank);
+            //Console.WriteLine("Card 2: " + card2.Rank);
             
 
             //Game game = new TwentyOneGame
@@ -30,8 +39,8 @@ namespace TwentyOneCardGame
             //game += player;
             //game.ListPlayers();
 
-            FullDeck deck = new FullDeck();
-            deck.Shuffle(2);
+            //FullDeck deck = new FullDeck();
+            //deck.Shuffle(2);
 
 
             ////Prints deck to the console
