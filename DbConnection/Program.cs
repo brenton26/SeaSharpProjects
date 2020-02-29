fa93eeb29a503712f3da0997ad832bd42a4563d8
Brenton <bren10@gmail.com>
2020-02-28 23:00:39 -0800

    new files

---
+++ DbConnection/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace nameDbConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to connect to a SQL database or an Orcale database? ");
+            Console.Write("Enter 1 for SQL or 2 for Oracle: ");
+            string user_input = Console.ReadLine();
+            if (user_input != "1" && user_input != "2") { Environment.Exit(420); }
+            Console.Write("Input configuration string: ");
+            string user_conn_input = Console.ReadLine();
+            if (user_input == "1")
+            {
+                Console.WriteLine("\nPlease type your command.");
+                Console.Write(">>> ");
+                string user_command = Console.ReadLine();
+                var conn = new SqlConnection(user_conn_input);
+                var command = new DbCommand(conn, user_command);
+                command.Execute();
+            }
+            if (user_input == "2")
+            {
+                Console.WriteLine("\nPlease type your command.");
+                Console.Write(">>> ");
+                string user_command = Console.ReadLine();
+                var conn = new OracleConnection(user_conn_input);
+                var command = new DbCommand(conn, user_command);
+                command.Execute();
+            }
+        }
+    }
+    public abstract class DbConnection
+    {
+        public DbConnection(string connection_string)
+        {
+            if (connection_string.Trim() == "")
+            {
+                throw new InvalidOperationException("This connection requires a connection string to be in a valid state.");
+            }
+            this.ConnectionString = connection_string;
+        }
+        private string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+    }
+
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connection_string) : base(connection_string) { }
+        public override void OpenConnection()
+        {
+            Console.WriteLine("SqlConnection: connected.");
+        }
+        public override void CloseConnection()
+        {
+            Console.WriteLine("SqlConnection: disconnected.");
+        }
+    }
+
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connection_string) : base(connection_string) { }
+        public override void OpenConnection()
+        {
+            Console.WriteLine("OracleConnection: connected.");
+        }
+        public override void CloseConnection()
+        {
+            Console.WriteLine("OracleConnection: disconnected.");
+        }
+    }
+
+    public class DbCommand
+    {
+        public DbCommand(DbConnection conn, string instruction)
+        {
+            this.Connection = conn;
+            this.Instruction = instruction;
+        }
+        private DbConnection Connection { get; set; }
+        public string Instruction { get; set; }
+
+        public void Execute()
+        {
+            Connection.OpenConnection();
+            Console.WriteLine("Executing the following instruction: " + Instruction);
+            Connection.CloseConnection();
+        }
+    }
+
+}
---
+++ InterfacesAndTests/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace InterfacesAndTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OrderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            OrderProcessor.Process(order);
+        }
+    }
+    // class OrderProcessor
+    public class OrderProcessor
+    {
+        private readonly IShippingCalculator _shippingCalculator;
+
+        public OrderProcessor(IShippingCalculator shippingCalculator)
+        {
+            _shippingCalculator = shippingCalculator;
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+            {
+                throw new InvalidOperationException("This order is already processed.");
+            }
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+    // interface IShippingCalculator
+    public interface IShippingCalculator
+    {
+        float CalculateShipping(Order order);
+    }
+    // class ShippingCalculator
+    public class ShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            if (order.TotalPrice >= 30f) { return 0; }
+            return order.TotalPrice * 0.1f;
+        }
+    }
+    // class Order
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DatePlaced { get; set; }
+        public Shipment Shipment { get; set; }
+        public float TotalPrice { get; set; }
+
+        public bool IsShipped
+        {
+            get { return Shipment != null; }
+        }
+
+    }// class Shipment
+    public class Shipment
+    {
+        public float Cost;
+        
+    }
+}
